b74127c17cecf7c04a72c963754030bad9cbed3f
intern <yuuki08163256@gmail.com>
2020-01-14 15:13:27 +0900

    11-12

--- app/src/main/java/com/websarva/wings/android/myinternaprication/ArticleSearch.java
+++ app/src/main/java/com/websarva/wings/android/myinternaprication/ArticleSearch.java
@@ -5,9 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.SimpleAdapter;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
